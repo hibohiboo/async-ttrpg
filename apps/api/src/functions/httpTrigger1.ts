b40e3b1fcb0b053737dfe9a5363e4b0598eae819
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-20 21:13:53 +0900

    add prettier #2

--- apps/api/src/functions/httpTrigger1.ts
+++ apps/api/src/functions/httpTrigger1.ts
@@ -3,17 +3,17 @@ import {
   HttpRequest,
   HttpResponseInit,
   InvocationContext,
-} from "@azure/functions";
-import { format } from "date-fns";
+} from '@azure/functions';
+import { format } from 'date-fns';
 
 export async function httpTrigger1(
   request: HttpRequest,
-  context: InvocationContext
+  context: InvocationContext,
 ): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
-  const name = request.query.get("name") || (await request.text()) || "world";
-  const time = format(new Date(), "yyyy-MM-dd: HH:mm:ss");
+  const name = request.query.get('name') || (await request.text()) || 'world';
+  const time = format(new Date(), 'yyyy-MM-dd: HH:mm:ss');
   return {
     body: JSON.stringify({
       msg: `Hello, ${name}!`,
@@ -22,8 +22,8 @@ export async function httpTrigger1(
   };
 }
 
-app.http("httpTrigger1", {
-  methods: ["GET", "POST"],
-  authLevel: "anonymous",
+app.http('httpTrigger1', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
   handler: httpTrigger1,
 });
