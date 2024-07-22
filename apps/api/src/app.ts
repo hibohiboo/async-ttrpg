878143070bcc8068c3eb84967e827760638b4454
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-22 23:49:08 +0900

    fix cors header #4

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,6 +3,14 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
 const app = new Hono()
+  .use(
+    '/api/*',
+    cors({
+      origin: ['*'],
+      allowHeaders: ['X-Custom-Header', 'Content-Type'],
+      allowMethods: ['POST', 'GET', 'OPTIONS'],
+    }),
+  )
   .get('/', (c) => c.text('Hello Azure Functions!'))
   .get('/api/httpTrigger1', async (c) => {
     const name = c.req.query('name') || (await c.req.text()) || 'world';
@@ -10,6 +18,7 @@ const app = new Hono()
     const time = format(new Date(), 'yyyy-MM-dd: HH:mm:ss');
     return c.json({ msg: `Hello, ${name}!`, time });
   });
+// https://hono.dev/docs/middleware/builtin/cors
 app.use('/api/*', cors());
 export default app;
 export type AppType = typeof app;
