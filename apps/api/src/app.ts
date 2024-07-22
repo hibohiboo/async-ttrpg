31f6cf707849d5dfefcbb593ac700ac4a466acea
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-22 23:43:29 +0900

    add cors #4

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,7 @@
 import { format } from 'date-fns';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
 const app = new Hono()
   .get('/', (c) => c.text('Hello Azure Functions!'))
   .get('/api/httpTrigger1', async (c) => {
@@ -8,6 +10,6 @@ const app = new Hono()
     const time = format(new Date(), 'yyyy-MM-dd: HH:mm:ss');
     return c.json({ msg: `Hello, ${name}!`, time });
   });
-
+app.use('/api/*', cors());
 export default app;
 export type AppType = typeof app;
