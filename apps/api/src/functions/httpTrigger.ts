b8c3eae3793a54431a8c41fb65190555057a1d78
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-22 22:55:56 +0900

    fix 型エラー #4

--- apps/api/src/functions/httpTrigger.ts
+++ apps/api/src/functions/httpTrigger.ts
@@ -1,10 +1,10 @@
 import { app } from '@azure/functions';
 import honoApp from '@api/app';
-import { azureHonoHandler } from '@api/utils/honoAzureFuncAdapter/azureHonoHandler';
-
+import { azureHonoHandler } from '@marplex/hono-azurefunc-adapter';
 app.http('httpTrigger', {
   methods: ['GET', 'POST'],
   authLevel: 'anonymous',
   route: '{*proxy}',
+  // fetchの引数にはcontextを渡すことができないので第２引数のcontextは失われる。context.logによるログ出力はできないが、関数が１つだけのため関数とログの紐づけができなくなっても影響はない。
   handler: azureHonoHandler(honoApp.fetch),
 });
