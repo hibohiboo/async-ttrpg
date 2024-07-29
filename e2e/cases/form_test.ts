b6f190001f05ea588c22de742f0ee4e68ca129fe
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-29 12:32:57 +0900

    fix 事前準備 #6

--- e2e/cases/form_test.ts
+++ e2e/cases/form_test.ts
@@ -1,5 +1,12 @@
 Feature('キャラクターのCRUD');
 
+Before(() => {
+  const exec = require('child_process').exec;
+  exec(
+    'docker exec -d mssql /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P MyPassword@123 -C -d test -Q "TRUNCATE TABLE atrpg.Character;"',
+  );
+});
+
 Scenario('キャラクターの追加', ({ I }) => {
   const 追加 = locate('a').withText('キャラクターを追加').as('追加');
   const Save = locate('button').withText('Save').as('Save');
