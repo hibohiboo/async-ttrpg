e3749d01b43e38e4fb5d8fc59efcdeab7f24e089
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-29 11:40:58 +0900

    fix 不要ファイル整理 #6

--- database/initdb.d/entrypoint.sh
+++ database/initdb.d/entrypoint.sh
@@ -4,7 +4,7 @@
 echo "Waiting for SQL Server to start..."
 /opt/mssql/bin/sqlservr & MSSQL_PID=$!
 
-# SQL Serverの起動を待機
+# SQL Serverの起動を待機. 起動中はログインに失敗するため、ログインに成功するまで待機する必要がある。 https://github.com/Microsoft/mssql-docker/issues/203
 while ! /opt/mssql-tools18/bin/sqlcmd -S $HOST_NAME -U sa -P $MSSQL_SA_PASSWORD -C -Q "SELECT 1" > /dev/null 2>&1; do
     sleep 1
 done
@@ -12,6 +12,7 @@ echo "SQL Server started."
 
 # 初期化用SQLを実行
 echo "Initializing database..."
+# hostnameでもlocalhostでもどちらでもアクセスできることのテスト
 /opt/mssql-tools18/bin/sqlcmd -S $HOST_NAME -U sa -P $MSSQL_SA_PASSWORD -C -d master -Q "CREATE DATABASE test;"
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -d test -Q "CREATE SCHEMA atrpg;"
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -d test -i /docker-initdb.d/ddl.sql
--- database/initdb.d/init.sh
+++
@@ -1,8 +0,0 @@
-
-echo "wait $WAIT_TIME_FOR_LOGIN seconds setup script"
-sleep $WAIT_TIME_FOR_LOGIN
-
-echo "running setup script"
-/opt/mssql-tools18/bin/sqlcmd -S $HOST_NAME -U sa -P $MSSQL_SA_PASSWORD -C -d master -Q "CREATE DATABASE test;"
-# /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -d test -Q "CREATE SCHEMA atrpg;"
-# /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -d test -i ddl.sql
