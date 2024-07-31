7d7f9af952f9b567f457577e8d9472e0817c908a
hibohiboo <hibohiboo66+github@gmail.com>
2024-08-01 01:15:37 +0900

    fix client path #8

--- apps/api/src/shared/prisma.ts
+++ apps/api/src/shared/prisma.ts
@@ -1,3 +1,3 @@
-import { PrismaClient } from '@db/client';
+import { PrismaClient } from '../../generated/client';
 // https://github.com/glaucia86/azure-sql-prisma-vue/blob/main/api/shared/prisma.js
 export const prisma = new PrismaClient();
