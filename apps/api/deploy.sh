74c2182dcabc23f9cafff9abb792134cfbb16719
hibohiboo <hibohiboo66+github@gmail.com>
2024-08-01 00:25:56 +0900

    fix pipeline #8

--- apps/api/deploy.sh
+++ apps/api/deploy.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 BIN_DIR=$(cd $(dirname $0) && pwd)
 BUILD_DIR=$BIN_DIR/build
-rimraf ./build
-rimraf ./dist
-npm run build
-mkdir $BUILD_DIR
-cp -r dist $BUILD_DIR
-cp host.json $BUILD_DIR
-cp .funcignore $BUILD_DIR
-cp local.settings.json $BUILD_DIR
-# package.json から devDependencies を削除して、本番環境用の node_modules を作成 (@async-ttrpg/typescript-configなどmonorepoの機能で参照しているパッケージがエラーを引き起こすため)
-jq 'del(.devDependencies)' package.json > temp.json && mv temp.json $BUILD_DIR/package.json
 
 # 事前にビルド
-npm run build
+bash build.sh
 
 cd $BUILD_DIR && npm install -omit=dev \
     && func azure functionapp publish $APP_NAME --subscription $AZURE_SUBSCRIPTION_ID
