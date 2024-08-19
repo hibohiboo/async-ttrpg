fab476cee894708a16ab5fb9650b0ce9e02cf1cb
hibohiboo <hibohiboo66+github@gmail.com>
2024-08-19 21:23:22 +0900

    add test

--- apps/api/src/routes/transactionTest.ts
+++ apps/api/src/routes/transactionTest.ts
@@ -3,10 +3,12 @@ import { zValidator } from '@hono/zod-validator';
 import { TransactionTestSchema } from '@db/zod';
 import Database from '@api/lib/database';
 import * as sql from 'mssql';
+import TestDatabase from '@api/lib/testdb';
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const connectionString = process.env.CONNECTION_STRING!;
 const db = new Database(connectionString); // 接続を使いまわすため、関数の外で定義
+const testdb = new TestDatabase(connectionString);
 console.log('connection', connectionString);
 const app = new Hono()
   .post(
@@ -66,6 +68,73 @@ const app = new Hono()
       });
 
       return c.json({});
+    },
+  )
+  .post(
+    '/lock',
+    zValidator('json', TransactionTestSchema.array().min(1)),
+    async (c) => {
+      const data = await c.req.valid('json');
+      await testdb.beginTransaction();
+      try {
+        const [{ TestID }] = data;
+        const deleteArgs = {
+          logger: console.log,
+          query: 'DELETE from atrpg.TransactionTest where TestID = @TestID',
+          params: [{ name: 'TestID', data: TestID, type: sql.NVarChar }],
+        };
+
+        const table = new sql.Table('atrpg.TransactionTest');
+        // prettier-ignore
+        table.columns.add('TestID', sql.NVarChar, { nullable: false, primary: true });
+        // prettier-ignore
+        table.columns.add('CreatedAt', sql.DateTimeOffset, { nullable: false, primary: true });
+        data.forEach((row) => {
+          table.rows.add(row.TestID, row.CreatedAt);
+        });
+
+        await testdb.executePreparedStatement(deleteArgs);
+        await testdb.bulk({ table, logger: console.log });
+        await testdb.commitTransaction();
+        return c.json({});
+      } catch (e) {
+        await testdb.rollbackTransaction();
+        throw e;
+      }
+    },
+  )
+  .post(
+    '/lock-delay',
+    zValidator('json', TransactionTestSchema.array().min(1)),
+    async (c) => {
+      const data = await c.req.valid('json');
+      await testdb.beginTransaction();
+      try {
+        const [{ TestID }] = data;
+        const deleteArgs = {
+          logger: console.log,
+          query: 'DELETE from atrpg.TransactionTest where TestID = @TestID',
+          params: [{ name: 'TestID', data: TestID, type: sql.NVarChar }],
+        };
+
+        const table = new sql.Table('atrpg.TransactionTest');
+        // prettier-ignore
+        table.columns.add('TestID', sql.NVarChar, { nullable: false, primary: true });
+        // prettier-ignore
+        table.columns.add('CreatedAt', sql.DateTimeOffset, { nullable: false, primary: true });
+        data.forEach((row) => {
+          table.rows.add(row.TestID, row.CreatedAt);
+        });
+
+        await testdb.executePreparedStatement(deleteArgs);
+        await new Promise((resolve) => setTimeout(resolve, 1000 * 10));
+        await testdb.bulk({ table, logger: console.log });
+        await testdb.commitTransaction();
+        return c.json({});
+      } catch (e) {
+        await testdb.rollbackTransaction();
+        throw e;
+      }
     },
   );
 
