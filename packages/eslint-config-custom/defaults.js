504251fde11306354d7bc938ccd51a43c5daaddf
hibohiboo <hibohiboo66+github@gmail.com>
2024-09-07 01:01:13 +0900

    chore update package #22

--- packages/eslint-config-custom/defaults.js
+++ packages/eslint-config-custom/defaults.js
@@ -5,7 +5,7 @@ import js from '@eslint/js';
 import { FlatCompat } from "@eslint/eslintrc";
 import unuserdPlugin from 'eslint-plugin-unused-imports';
 import prettierConfig from 'eslint-config-prettier';
-
+import sonarjs from "eslint-plugin-sonarjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,8 +16,13 @@ const compat = new FlatCompat({
 
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended,...compat.extends('eslint-config-turbo'), prettierConfig],
-  plugins: { 'unused-imports': unuserdPlugin },
+  extends: [ js.configs.recommended
+            , ...tseslint.configs.recommended
+            , sonarjs.configs.recommended
+            , ...compat.extends('eslint-config-turbo')
+            , prettierConfig
+          ],
+  plugins: { 'unused-imports': unuserdPlugin, sonarjs },
   rules: {
     semi: ['error', 'always'],
     complexity: ['error', 10], // 複雑度の設定
