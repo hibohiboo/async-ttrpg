ac9b964ce06059cedf3e814e5d77f0907b6b1d50
hibohiboo <hibohiboo66+github@gmail.com>
2024-09-28 00:57:49 +0900

    fix delete deploy #56

--- apps/api/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BIN_DIR=$(cd $(dirname $0) && pwd)
-BUILD_DIR=$BIN_DIR/build
-
-# 事前にビルド
-npm run build
-
-cd $BUILD_DIR && func azure functionapp publish $APP_NAME --subscription $AZURE_SUBSCRIPTION_ID
-
