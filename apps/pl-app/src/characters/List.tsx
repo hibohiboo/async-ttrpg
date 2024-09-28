e5e89d762a520ee6febd80a1024ce3081a2251fa
hibohiboo <hibohiboo66+github@gmail.com>
2024-09-28 21:11:32 +0900

    fix lint apps #22

--- apps/pl-app/src/characters/List.tsx
+++ apps/pl-app/src/characters/List.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import { Character, characterService } from '../_services/character.service';
 
 function List() {
