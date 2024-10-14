0578359df6d6fab7d19a8fd0384d7190e589517b
hibohiboo <hibohiboo66+github@gmail.com>
2024-10-14 20:26:14 +0900

    fix path

--- apps/pl-app/src/router/index.tsx
+++ apps/pl-app/src/router/index.tsx
@@ -1,10 +1,15 @@
-import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { createBrowserRouter, Navigate, Outlet } from 'react-router-dom';
 import { CharacterEdit, CharacterList } from '@pl-app/features/characters';
 import { CharacterPage } from '@pl-app/pages/character-page';
 import { AsyncTrpgLayout } from '@pl-app/widgets/layout';
 
 export const createRouter = () =>
   createBrowserRouter([
+    {
+      path: '/',
+      // replaceプロパティを使用して、ブラウザの履歴を置き換え。これにより、ユーザーが「戻る」ボタンを押したときにリダイレクト前のページに戻らないようにする。
+      element: <Navigate to="/characters" replace />,
+    },
     {
       element: (
         <AsyncTrpgLayout>
